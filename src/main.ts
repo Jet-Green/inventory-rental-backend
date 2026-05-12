f2726ef75417a53fa0bc211237d12cad9c190e22
Jet-Green <grishadzyin@gmail.com>
2026-05-12 14:22:08 +0500

    a

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.CLIENT_URL || "http://localhost:3068"],
+    origin: [process.env.CLIENT_URL || "https://rent.firetechno.ru"],
     credentials: true,
   });
   app.use(cookieParser());
